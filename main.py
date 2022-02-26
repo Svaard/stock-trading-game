03b6666ccbd29e0b80e2481a6a66b7d6809d4095
Svaard <daniel.n.flores@gmail.com>
2022-02-26 01:58:24 -0800

    Updates to main

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import discord
 import config
-
-token: str = config.TOKEN
+import re
 
 class MyClient(discord.Client):
+
+    def __init__(self):
+        super().__init__()
+        self.token = self.get_token()
+        self.run(self.token)
+
+    def get_token(self):
+        return config.TOKEN
+
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(f'Logged on as {self.user} with  id {self.user.id}')
 
     async def on_message(self, message):
-        author: str = message.author
-        server: str = message.guild
-        content: str = message.content
-        print(f'{author} {server} {content}')
+        content = message.clean_content
+        content = re.sub(r'<(:[\w\d]+:)\d{18}>', r'\1', content)
+        if type(message.channel) == discord.DMChannel:
+            print(f'DM [{message.author}]: {message.content}')
+        else:
+            print(f'{message.channel.guild}#{message.channel} [{message.author}]: {content}')
 
         # don't respond to ourselves
-        if message.author == self.user:
+        if message.author.bot:
             return
 
         if message.content == 'ping':
             await message.channel.send('pong')
 
-client = MyClient()
-client.run(token)
+MyClient()
